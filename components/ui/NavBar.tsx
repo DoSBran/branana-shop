f377c06a6e5ab15fb97b34834940188979fa8b01
brandoncfridivi <96086960+brandoncfridivi@users.noreply.github.com>
2023-05-25 16:29:26 -0600

    estructura de carpetas, shopLayout y navbar

---
+++ components/ui/NavBar.tsx
@@ -0,0 +1,62 @@
+import {
+  AppBar,
+  Badge,
+  Box,
+  Button,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import NextLink from "next/link";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
+export const NavBar = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        <NextLink href="/" passHref legacyBehavior>
+          <Link display="flex" alignItems="center">
+            <Typography variant="h6">Branana |</Typography>
+            <Typography sx={{ ml: 0.5 }}>Shop</Typography>
+          </Link>
+        </NextLink>
+        <Box flex={1} />
+        <Box sx={{display: {xs: 'none', sm: 'block'}}}>
+          <NextLink href="/category/men" passHref legacyBehavior>
+            <Link>
+              <Button>Hombres</Button>
+            </Link>
+          </NextLink>
+          <NextLink href="/category/women" passHref legacyBehavior>
+            <Link>
+              <Button>Mujeres</Button>
+            </Link>
+          </NextLink>
+          <NextLink href="/category/kid" passHref legacyBehavior>
+            <Link>
+              <Button>Niños</Button>
+            </Link>
+          </NextLink>
+        </Box>
+        <Box flex={1} />
+
+        <IconButton>
+          <SearchOutlinedIcon />
+        </IconButton>
+
+        <NextLink href="cart" passHref legacyBehavior>
+          <Link>
+            <IconButton>
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCartOutlinedIcon />
+              </Badge>
+            </IconButton>
+          </Link>
+        </NextLink>
+        <Button>Menu</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
